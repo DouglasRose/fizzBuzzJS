5ec9a69c3d1d41754ccfc77960f25adeb48939ba
Douglas Rose <douglahr@gmail.com>
2016-02-16 18:16:54 +0100

    Finished FizzBuzz in JavaScript

--- spec/FizzJuiceSpec.js
+++ spec/FizzJuiceSpec.js
@@ -18,5 +18,21 @@ describe('FizzJuice', function() {
     it('divisible by 15', function() {
       expect(fizzjuice.isDivisibleByFifteen(45)).toBe(true);
     });
+
+    it('returns "Fizz" if number is divisible by three', function() {
+      expect(fizzjuice.response(3)).toBe("Fizz");
+    });
+
+    it('returns "Juice" if number is divisible by five', function() {
+      expect(fizzjuice.response(5)).toBe("Juice");
+    });
+
+    it('returns "FizzJuice" if number is divisible by fifteen', function() {
+      expect(fizzjuice.response(15)).toBe("FizzJuice");
+    });
+
+    it('returns itself if number is not divisible by 3, 5, 15', function() {
+      expect(fizzjuice.response(4)).toBe("4");
+    });
   });
 });
--- src/FizzJuice.js
+++ src/FizzJuice.js
@@ -3,17 +3,31 @@ function FizzJuice() {
 };
 
 FizzJuice.prototype.isDivisibleByThree = function(number) {
-  return isDivisibleBy(number, 3);
+  return this.isDivisibleBy(number, 3);
 };
 
 FizzJuice.prototype.isDivisibleByFive = function(number) {
-  return isDivisibleBy(number, 5);
+  return this.isDivisibleBy(number, 5);
 };
 
 FizzJuice.prototype.isDivisibleByFifteen = function(number) {
-  return isDivisibleBy(number, 15);
+  return this.isDivisibleBy(number, 15);
 };
 
 FizzJuice.prototype.isDivisibleBy = function(number, divisor) {
   return number % divisor === 0;
+};
+
+FizzJuice.prototype.response = function(number) {
+  if (this.isDivisibleByFifteen(number))
+  return "FizzJuice";
+
+  else if (this.isDivisibleByFive(number))
+  return "Juice";
+
+  else if (this.isDivisibleByThree(number))
+    return "Fizz";
+
+  else
+    return String(number);
 };
